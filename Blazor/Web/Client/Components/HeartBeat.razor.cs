12a33f2f50c8996666e8532f3c86c58709648c56
Christopher Willoch <ulveie@msn.com>
2022-11-08 15:26:58 +0100

    Forex page works

---
+++ Blazor/Web/Client/Components/HeartBeat.razor.cs
@@ -0,0 +1,55 @@
+﻿using Messages.Client;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Web.Client.Components
+{
+    public partial class HeartBeat:ComponentBase,IDisposable
+    {
+        private CancellationTokenSource? CSource { get; set; }
+        [Inject]
+        private IUIBus? UIBus { get; set; }
+        private static readonly int StartingInterval = 5000;
+        private static readonly double BackOffFactor = 1.1;
+        private double interval = StartingInterval;
+        private int Interval
+        {
+            get
+            {
+                int i = (int)interval;
+                interval = interval * BackOffFactor;
+                return i;
+            }
+        }
+        public void Dispose()
+        {
+            CSource?.Cancel();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            CSource = new CancellationTokenSource();
+            _ = EmitHeartBeat(CSource.Token);
+        }
+        private Task EmitHeartBeat(CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return Task.CompletedTask;
+            int ms = Interval;
+            UIBus?.Notify(new HeartBeatNotification(ms));
+           
+            return Task.Run(async () => { await Task.Delay(ms, token); return EmitHeartBeat(token); });
+        }
+        public class HeartBeatNotification : IUiBusEvent 
+        {
+            public int Frequency { get; }
+            internal HeartBeatNotification(int frequency)
+            {
+                this.Frequency = frequency;
+            }
+        }
+    }
+}
---
+++ Blazor/Web/Shared/Models/ICurrencyPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Shared.Models
+{
+    public interface ICurrencyPair
+    {
+        string? FirstTicker { get; }
+        string? SecondTicker { get; }
+    }
+}
