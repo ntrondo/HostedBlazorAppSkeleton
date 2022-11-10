f1a175b32d970d39201fc574973231a613fe6a64
Christopher Willoch <ulveie@msn.com>
2022-11-10 15:44:30 +0100

    Added Component test project with bunit

---
+++ Tests/UnitTests/Models/MockEventListener.cs
@@ -0,0 +1,25 @@
+﻿using Messages.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    internal class MockEventListener : IListener<MockEvent>
+    {
+        public List<IUiBusEvent> Events { get; } = new List<IUiBusEvent>();
+        public MockEventListener()
+        {
+        }
+        public void Dispose()
+        {
+        }
+
+        public void Handle(MockEvent theEvent)
+        {
+            this.Events.Add(theEvent);
+        }
+    }
+}
